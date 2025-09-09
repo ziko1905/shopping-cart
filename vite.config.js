86b763c48c45c017a75cf8b0cf15b5ee521eba83
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-09 23:57:39 +0200

    Configure tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/tests/setup.js"
-  }
-})
+    setupFiles: "./src/tests/setup.js",
+  },
+});
