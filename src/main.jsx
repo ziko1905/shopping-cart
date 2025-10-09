989412bf8c81069a809e97fc0892a8823b1b70ab
Karlo Cehulic <karlocehlic@gmail.com>
2025-10-09 22:39:58 +0200

    Remove css resetter
    Not needed bcs tailwind has its own

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.jsx";
 import "./index.css";
-import "./default.css";
 
 const router = createBrowserRouter(routes);
 
